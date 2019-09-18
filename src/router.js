3fb7a089546c22d5f141f0af46bf7b3b8c38c597
wxj-9527 <691803039@qq.com>
2019-09-18 22:22:49 +0800

    登录模块及布局样式

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/home/'
+import Login from './views/login'
 
 Vue.use(Router)
 
@@ -8,8 +9,17 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
       component: Home
+    },
+    {
+      path: '/login',
+      component: Login
     }
+
     // {
     //   path: '/about',
     //   name: 'about',
---
+++ src/views/login/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="login">
+      <el-card class="login-card">
+          <div class="title">
+              <img src="../../assets/img/logo_index.png" alt="">
+          </div>
+          <el-form style="margin-top:10px">
+              <el-form-item>
+                  <el-input></el-input>
+              </el-form-item>
+              <el-form-item>
+                  <el-input style="width:65%"></el-input>
+                  <el-button style="float:right">发送验证码</el-button>
+              </el-form-item>
+              <el-form-item>
+                  <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+              </el-form-item>
+              <el-form-item>
+                  <el-button type="primary" style="width:100%">登录</el-button>
+              </el-form-item>
+          </el-form>
+      </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.login {
+    background-image: url('../../assets/img/login_bg.jpg');
+    // width: 100%;
+    height: 100vh;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-card {
+        width: 440px;
+        height: 340px;
+        .title{
+            text-align: center;
+            img{
+                height: 45px;
+            }
+        }
+    }
+}
+</style>
