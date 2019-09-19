d8856444cac1f2637fe9e850d3669930f462f204
wxj-9527 <691803039@qq.com>
2019-09-19 17:55:23 +0800

    左侧导航

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>我是主页</div>
+  <el-container>
+    <el-aside>
+      <layout-aside></layout-aside>
+    </el-aside>
+    <el-container>
+      <el-header>头部</el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
